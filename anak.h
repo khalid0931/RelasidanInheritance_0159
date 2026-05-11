7255fa4adacf5cc95c9b51b82b62fc78e920f606
khalid0931 <mutiarif53@gmail.com>
2026-05-11 19:19:31 +0700

    membuat class anak

--- anak.h
+++ anak.h
@@ -7,3 +7,7 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
